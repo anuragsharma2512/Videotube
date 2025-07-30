329bc046ed61ac12579e58c0deed7efd1b3fc01a
anuragsharma2512 <anuragsharma4213.com>
2025-07-30 22:40:10 +0530

    doing Healthcheck routes and testing

--- src/app.js
+++ src/app.js
@@ -14,4 +14,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// import routes
+import healthCheckRoutes from "./routes/healthcheck.routes.js";
+
+// use routes
+
+app.use("/api/v1/healthcheck", healthCheckRoutes);
+
 export { app };
